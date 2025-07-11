e78af02c8c8f2d10d4ac533d85715139cbb3c379
santhu643 <santhumoni04@gmail.com>
2025-07-11 12:23:37 +0530

    String operator

---
+++ Strings/operators.java
@@ -0,0 +1,9 @@
+public class operators{
+    public static void main(String args[]){
+         System.out.println('a'+'b');
+         System.out.println("a"+"b");
+         System.out.println((char)('a'+1));
+         System.out.println("a"+1);
+
+    }
+}
