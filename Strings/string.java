67cc52e352aee913d6931ce60b6a23ea8f988324
santhu643 <santhumoni04@gmail.com>
2025-07-11 10:53:53 +0530

    heap pool

---
+++ Strings/string.java
@@ -0,0 +1,10 @@
+public class string{
+    public static void main(String args[]){
+         String a="Santhu";
+         //immutable
+         String b="Santhu";
+         System.out.println(a);
+         a="Santhiya";
+         System.out.println(a);
+    }
+}
--- cyclicsort.java
+++ cyclicsort.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class cyclicsort{
     public static void main (String []args){
-        int[] arr = {5,3,2,4,1};
+        int[] arr = {5,3,10,25,2,4,1};
         cycle(arr);
         System.out.print(Arrays.toString(arr));
     }
