5b910669bd1dd2058c196c9c5f5555316e375716
santhu643 <santhumoni04@gmail.com>
2025-07-11 12:40:48 +0530

    String methods

---
+++ Strings/methods.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class methods{
+    public static void main(String args[]){
+        String name="Santhiya";
+        String b="santhiyaprakash";
+         System.out.println(Arrays.toString(name.toCharArray()));
+         System.out.println(name.toLowerCase());
+         System.out.println(name.toUpperCase());
+         System.out.println(name.indexOf('a'));
+         System.out.println(" Santhiya ".strip());
+         System.out.println(Arrays.toString(b.split("a")));
+    }
+}
