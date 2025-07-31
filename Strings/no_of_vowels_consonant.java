46cf2462c34a98ef1d912c8a41e7502f8b077a08
santhu643 <santhumoni04@gmail.com>
2025-07-31 14:00:31 +0530

    vowels and consonant

---
+++ Strings/no_of_vowels_consonant.java
@@ -0,0 +1,19 @@
+public class no_of_vowels_consonant{
+    public static void main(String args[]){
+        String str ="hello world";
+        check(str);
+    }
+
+    public static void check(String str){
+        int vowel = 0;
+        int consonant = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+                vowel+=1;
+            }else if(str.charAt(i)!=' '){
+                consonant+=1;
+            }
+        }
+        System.out.print(vowel + " "+ consonant);
+    }
+}
