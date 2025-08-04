e6316e4cf4a1384431d0a9aa79bbe396ba134b26
santhu643 <santhumoni04@gmail.com>
2025-08-04 14:25:44 +0530

    oops

--- OOPS_Training/cow.java
+++ OOPS_Training/cow.java
@@ -2,16 +2,16 @@ public class cow{
      public static void main(String args[]){
         TestCow c1 = new TestCow();
         TestCow c2 = c1;
-        System.out.println(c1.name="santhiya");
+        System.out.println(c1.name="dhaku");
         c1.moo();
         System.out.println(c2.name="santhu");
         System.out.println(c1.name);
+        c2.moo();
     }
 }
 
 class TestCow{
     String name;
-    int a;
     public void moo(){
         System.out.println("I am cow and I am moo mooing "+name);
     }
