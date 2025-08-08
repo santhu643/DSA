17be551ba194128d78f77507a3cd651429fad9d1
santhu643 <santhumoni04@gmail.com>
2025-08-08 11:28:04 +0530

    dsa

--- prgm__old/insertionsort.java
+++ prgm__old/insertionsort.java
@@ -20,3 +20,28 @@ public class InsertionSortSwap {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+
+// //Insertion Sort
+// public class Main {
+//     public static void main(String[] args) {
+//         int[] arr = {7, 5, 3, 2, 4, 6};
+//         int n = arr.length;
+
+//         for (int i = 1; i < n; i++) {
+//             int key = arr[i];     
+//             int j = i - 1;
+
+//             while (j >= 0 && arr[j] > key) {
+//                 arr[j + 1] = arr[j];  
+//                 j--;
+//             }
+
+//             arr[j + 1] = key;  
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
